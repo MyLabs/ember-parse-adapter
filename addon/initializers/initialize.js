bd5cc0225f0f8feaf4c54f8154100d3606a9d1a1
Omair Vaiyani <omairvaiyani@users.noreply.github.com>
2018-03-31 12:09:34 +0100

    Update initializer for Ember 3.0.0 compatability

--- addon/initializers/initialize.js
+++ addon/initializers/initialize.js
@@ -9,18 +9,17 @@ import ParseUser from "../models/parse-user";
  * @module initializers
  * @class  initialize
  */
-export default function( container, app ) {
-  Adapter.reopen({
-    host            : app.get( "parseUrl" ),
-    namespace       : app.get( "parseNamespace" ),
-    applicationId   : app.get( "applicationId" ),
-    restApiId       : app.get( "restApiId" )
-  });
-
-  container.register( "adapter:-parse", Adapter );
-  container.register( "serializer:-parse", Serializer );
-  container.register( "transform:parse-date", DateTransform );
-  container.register( "transform:parse-file", FileTransform );
-  container.register( "transform:parse-geo-point", GeopointTransform );
-  container.register( "model:parse-user", ParseUser );
+export default function( app ) {
+	Adapter.reopen({
+		host            : app.get( "parseUrl" ),
+		namespace       : app.get( "parseNamespace" ),
+		applicationId   : app.get( "applicationId" ),
+		restApiId       : app.get( "restApiId" )
+	});
+	app.register( "adapter:-parse", Adapter );
+	app.register( "serializer:-parse", Serializer );
+	app.register( "transform:parse-date", DateTransform );
+	app.register( "transform:parse-file", FileTransform );
+	app.register( "transform:parse-geo-point", GeopointTransform );
+	app.register( "model:parse-user", ParseUser );
 }
